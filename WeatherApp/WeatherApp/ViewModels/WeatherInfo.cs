58bf6fc7052079f5ceb5dcadf66224aa9cdad075
Kuni <kuni.ujiie@grapecity.com>
2018-01-11 16:33:48 +0900

    Updated.

---
+++ WeatherApp/WeatherApp/ViewModels/WeatherInfo.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public class WeatherInfo
+    {
+        public static RootObject GetDescription(City city)
+        {
+            string url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + city.ID.ToString();
+            WebClient webClient = new WebClient();
+            string str = webClient.DownloadString(url);
+            var weather = JsonConvert.DeserializeObject<RootObject>(str);
+
+            return weather;
+        }
+
+        public static List<Forcast> GetForcast(City city)
+        {
+            string url2 = "http://weather.livedoor.com/forecast/rss/area/" + city.ID.ToString() + ".xml";
+            WebClient webClient2 = new WebClient();
+            webClient2.Encoding = System.Text.Encoding.UTF8;
+            string str2 = webClient2.DownloadString(url2);
+            XDocument xd = XDocument.Parse(str2);
+            var ForcastList = new List<Forcast>();
+
+            IEnumerable<XElement> xelist = xd.XPathSelectElements("//item[category='天気予報']");
+            foreach (XElement el in xelist)
+            {
+                var Result = new Forcast();
+                Result.description = el.Element("description").Value.ToString();
+                var uri = new Uri(el.Element("image").Element("url").Value);
+                var img = new Image { Source = ImageSource.FromUri(uri) };
+                //img.Source = ImageSource.FromUri(uri);
+                Result.icon = img;
+
+                ForcastList.Add(Result);
+            }
+
+            return ForcastList;
+        }
+    }
+}
--- WeatherApp/WeatherApp/Views/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/MainPage.xaml.cs
@@ -23,42 +23,31 @@ namespace WeatherApp
             cbxCity.ItemsSource = list;
             cbxCity.DisplayMemberPath = "CityName";
             cbxCity.SelectedIndexChanged += CbxCity_SelectedIndexChanged;
+
+            // 初期表示は仙台
+            var sendai = new City();
+            sendai.CityName = "仙台";
+            sendai.ID = "040010";
+            cbxCity.Text = sendai.CityName;
+            cbxCity.SelectedItem = sendai;
+
+            // 初期表示。仙台の天気概況を取得
+            lblDesc.Text = WeatherInfo.GetDescription(sendai).description.text;
+
+            // 初期表示。仙台の天気予報を取得
+            grid.ItemsSource = WeatherInfo.GetForcast(sendai);
+
         }
 
         private void CbxCity_SelectedIndexChanged(object sender, EventArgs e)
         {
             var selectcity = cbxCity.SelectedItem as City;
 
-            // 天気概況を取得(JSON)
-            string url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + selectcity.ID.ToString();
-            WebClient webClient = new WebClient();
-            string str = webClient.DownloadString(url);
-            var weather = JsonConvert.DeserializeObject<RootObject>(str);
-            lblDesc.Text = weather.description.text;
+            // 天気概況を取得
+            lblDesc.Text = WeatherInfo.GetDescription(selectcity).description.text;
 
-            // 天気予報を取得(XML)
-            string url2 = "http://weather.livedoor.com/forecast/rss/area/" + selectcity.ID.ToString() + ".xml";
-            WebClient webClient2 = new WebClient();
-            webClient2.Encoding = System.Text.Encoding.UTF8;
-            string str2 = webClient2.DownloadString(url2);
-            XDocument xd = XDocument.Parse(str2);
-            var ForcastList = new List<Forcast>();
-
-            IEnumerable<XElement> xelist = xd.XPathSelectElements("//item[category='天気予報']");
-            foreach (XElement el in xelist)
-            {
-                Debug.WriteLine(el);
-                var Result = new Forcast();
-                Result.description = el.Element("description").Value.ToString();
-                var uri = new Uri(el.Element("image").Element("url").Value);
-                var img = new Image { Source = ImageSource.FromUri(uri) };
-                //img.Source = ImageSource.FromUri(uri);
-                Result.icon = img;
-
-                ForcastList.Add(Result);
-            }
-
-            grid.ItemsSource = ForcastList;
+            // 天気予報を取得
+            grid.ItemsSource = WeatherInfo.GetForcast(selectcity);
         }
     }
 }
